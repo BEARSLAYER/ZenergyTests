a9fd83c39a113c352abaf8d5377c95156d137079
BEARSLAYER <ejandre2@uncg.edu>
2013-03-25 19:03:03 -0400

    Added comments!

--- src/zenergytests/ZenergyTests.java
+++ src/zenergytests/ZenergyTests.java
@@ -4,7 +4,7 @@ package zenergytests;
 public class ZenergyTests {
     
     public static void main(String[] args) {
-        //CustomerServiceContactUsProductsTest test1 = new CustomerServiceContactUsProductsTest();
+        CustomerServiceContactUsProductsTest test1 = new CustomerServiceContactUsProductsTest();
         Purchase_Clearance_AsIs_Item_checkout test2 = new Purchase_Clearance_AsIs_Item_checkout();
     }
 }
